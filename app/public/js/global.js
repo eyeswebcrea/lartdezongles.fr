c2e75b633bbac863b91b5cdae59e4331612617b6
Your Name <jpasqualini75@gmail.com>
2012-05-30 18:03:39 +0200

    Ajout du slide auto et augmentation du temps entre les images

--- app/public/js/global.js
+++ app/public/js/global.js
@@ -2,5 +2,11 @@
  * @author john
  */
   $(function(){
-    $("div.slides").slides();
+    $("div#conteneur-bandeau div.slides").slides({
+    	play: 10000,
+    	slideSpeed: 1000,
+    	crossfade: true
+    });
+    
+    $("div#conteneur-bas div.slides").slides();
   });
